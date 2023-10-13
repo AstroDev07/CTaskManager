b874e615c1329e7dca7392300b1443835f503f91
Dantescur <daniel@thedaniweb.eu.org>
2023-10-13 02:39:59 -0400

    Refinado el script newtask.sh para mejorar la usabilidad: - Se agregaron validaciones de entrada para el título y la descripción. - El nombre del archivo generado ahora es más amigable con guiones bajos en lugar de espacios y en minúsculas. - Se proporcionan mensajes de error en rojo y mensajes de éxito en verde para una mejor retroalimentación visual. - Se ha agregado un registro de acciones en el archivo ~/.tasks.log.

--- src/newtask.sh
+++ src/newtask.sh
@@ -1,14 +1,44 @@
 cd ..
 cd .tasks
-echo "Diga el titulo de la tarea: "
+
+# Definir códigos de escape para colores de texto
+ROJO="\e[31m"
+VERDE="\e[32m"
+AMARILLO="\e[33m"
+RESETEAR="\e[0m"
+
+echo -e "${AMARILLO}Diga el título de la tarea: ${RESETEAR}"
 read title
-echo "Diga la descripcion de la tarea: "
-read description
-if [ -f "$title.txt" ];
-then
-	echo "Ya la tarea existe"
-else
-	echo $description > "$title.txt"
-	echo "Se creo la tarea"
+
+# Validación de entrada
+if [ -z "$title" ] || [ -z "${title// }" ]; then
+    echo -e "${ROJO}El título no puede estar vacío.${RESETEAR}"
+    exit 1
 fi
-return 0
+
+# Generar un nombre más amigable para el archivo (en minúsculas y con guiones bajos en lugar de espacios)
+file_name="$(echo $title | tr ' ' '_' | tr '[:upper:]' '[:lower:]').txt"
+
+# Comprobar si la tarea ya existe
+if [ -f "$file_name" ]; then
+    echo -e "${ROJO}La tarea ya existe.${RESETEAR}"
+    exit 1
+else
+    echo -e "${AMARILLO}Diga la descripción de la tarea: ${RESETEAR}"
+    read description
+
+    # Validación de entrada
+    if [ -z "$description" ] || [ -z "${description// }" ]; then
+        echo -e "${ROJO}La descripción no puede estar vacía.${RESETEAR}"
+        exit 1
+    fi
+
+    # Crear el archivo de la tarea
+    echo $description > "$file_name"
+    echo -e "${VERDE}Se creó la tarea.${RESETEAR}"
+
+    # Registrar la acción
+    echo "Nueva tarea creada: $title" >> ~/.tasks.log
+fi
+
+exit 0
